09e36444ac887b9f482814b7d855f70b7ee124c9
kuchaw <lzcn.inaki@gmail.com>
2023-11-16 17:50:09 -0300

    init

---
+++ alien_invasion.py
@@ -0,0 +1,40 @@
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+
+def run_game():
+    #initialize game and create a screen object
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien invasion")
+    #make a ship, a group of bullets and a group of aliens
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    #create the fleet of aliens
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    
+    #start the main loop for the game
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(aliens, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+
+
+
+        #redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()        
+        #make the most recentrly drwan screen visible
+        pygame.display.flip()
+
+run_game()
---
+++ settings.py
@@ -0,0 +1,23 @@
+class Settings():
+    """a class to store all settings for alien invasion"""
+
+    def __init__(self):
+        """initialize the game's settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0) 
+        #ship settings
+        self.ship_speed_factor = 1.5
+        #bullet settings
+        self.bullet_speed_factor = 50
+        self.bullet_width = 500
+        self.bullet_height = 15
+        self.bullet_color = 255, 0, 0
+        self.bullets_allowed = 5
+        #alien settings
+        self.alien_speed_factor = 1
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right; -1 represents
+        self.fleet_direction = 1
+
