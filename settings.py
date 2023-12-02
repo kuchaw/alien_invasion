ed24bd648f38ac1ef5f1024ed913cb4b06d5012f
kuchaw <lzcn.inaki@gmail.com>
2023-12-02 16:24:05 -0300

    cha cha real smooth

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ class Settings():
     """a class to store all settings for alien invasion"""
 
     def __init__(self):
-        """initialize the game's settings"""
+        """initialize the game's static settings"""
         #Screen settings
         self.screen_width = 1200
         self.screen_height = 800
@@ -17,8 +17,25 @@ class Settings():
         self.bullet_color = 255, 0, 0
         self.bullets_allowed = 5
         #alien settings
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
-        #fleet_direction of 1 represents right; -1 represents
+        #how quickly the game speeds up
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        """initialize settings that change trhoought the game"""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+
+        #fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
+
+    def increase_speed(self):
+        """increase speed settings"""
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale
+    
 
